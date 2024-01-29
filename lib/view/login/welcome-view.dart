f16c8398ee45682246a2e42ceba197e15f17197b
Tanmay kushwaha <tanmaykushwaha0101@gmail.com>
2024-01-29 09:17:02 +0530

    Some changes on Get Started page

--- lib/view/login/welcome-view.dart
+++ lib/view/login/welcome-view.dart
@@ -11,8 +11,41 @@ class WelcomeView extends StatefulWidget {
 }
 
 class _WelcomeViewState extends State<WelcomeView> {
+  PageController controller = PageController();
+  int selectPage = 0;
+
+  List pageArr = [
+    {
+      'title': 'Fun with Hunger',
+      'subtitle': 'Explore the Flavour and deals. \n Multiple ways to order',
+      'image': 'assets/images/handbags.jpg'
+    },
+    {
+      'title': 'Simple Steps',
+      'subtitle':
+          ' 1. Add items to your cart. \n 2. Quote for large order, if applicable \n 3. Pay in the app.',
+      'image': 'assets/images/mobile.png'
+    },
+    {
+      'title': 'Pick up & Have Fun with Hunger',
+      'subtitle': 'Create your Fhunger profile for \n rewards and deals.',
+      'image': 'assets/images/handbags.jpg',
+    },
+  ];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(() {
+      setState(() {
+        selectPage = controller.page?.floor() ?? 0;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
     return Scaffold(
         body: Column(
       children: [
@@ -47,26 +80,57 @@ class _WelcomeViewState extends State<WelcomeView> {
         const SizedBox(
           height: 70,
         ),
-        Image.asset(
-          'assets/images/handbags.jpg',
-          height: 220,
+        Expanded(
+          child: PageView.builder(
+              scrollDirection: Axis.horizontal,
+              controller: controller,
+              itemCount: pageArr.length,
+              itemBuilder: (context, index) {
+                var pObj = pageArr[index] as Map? ?? {};
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: media.width,
+                      constraints: const BoxConstraints(maxHeight: 250, minHeight: 250,),
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        pObj['image'].toString(),
+                        width: media.width * 0.6,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+
+                    Text(
+                      pObj['title'].toString(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w800,
+                        fontSize: 20,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 8,),
+                    Text(
+                      pObj['subtitle'].toString(),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 14,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                );
+              }),
         ),
-        const SizedBox(
-          height: 30,
-        ),
-        const Text(
-          'Pickup & Have Fun with Hunger',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 8.0),
-          child: Text(
-              textAlign: TextAlign.center,
-              'Create your fhunger profile for \n reward and fun deadatals',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-              )),
-        ),
+        // Image.asset(
+        //   'assets/images/handbags.jpg',
+        //   height: 220,
+        // ),
+
         const SizedBox(
           height: 70,
         ),
